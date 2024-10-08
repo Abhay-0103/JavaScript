f6d25e99f874263db8f04cfbb4f3c5d4ee87609d
Abhay Singh <ab0321054@gmail.com>
2024-10-08 09:58:10 +0000

    container comfig done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = "12133"
+let accountEmail = "lucky01@gmail.com"
+var accountPasswoard = "helloJi"
+accountCity = "Bangaluru"
+
+
+// accountId = 12323 // not allowed bcx we have constent 
+accountEmail = "abhay0103@gamail.com"
+accountPasswoard = "hihello"
+accountCity = "mumbai"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPasswoard,accountCity]);
+
