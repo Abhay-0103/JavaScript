b9c341542811ac5267e20ab41ff9fa201be2dc77
Abhay Singh <ab0321054@gmail.com>
2024-10-11 07:28:09 +0000

    Comparison

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,32 @@
+// mainly used in boolean and greater then or lesser then
+
+// console.log(2 > 1);
+// console.log(2 >=1);
+// console.log(2 < 1);
+// console.log(2 == 2);
+// console.log(2 !=1);
+
+
+
+// much use for camparing if it has same value and type
+// console.log("2" > 1);
+//console.log("02" > 1);
+
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// the first 2 null are false bcz its null is not big or small and thrid null is true bcz its can big or equal bas bhai bhai ab nahi 
+
+
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+// this undefind is always false bcz it not a number its undefind bas 
+
+
+// ===
+console.log("2" === 2);
+// this is strick check bcz it will deep check and not type is not same so its not true
