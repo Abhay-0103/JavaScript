a73e2ee14f16017934c1f4846df678289d21fd71
Abhay Singh <ab0321054@gmail.com>
2024-10-11 06:33:29 +0000

    Conversions Of DataType

---
+++ 01_Basics/03_ConversionOpertions.js
@@ -0,0 +1,49 @@
+let score = true
+ // if i use " " the number it will be string 
+
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+
+// now when we used " " it is now a string then now we will convert it into number by using operterions used below
+
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+
+
+// now when we use "33abc" it will show the same number so now we will check again
+
+
+// console.log(valueInNumber)
+
+
+ // NaN measn Not a number 
+
+ // if in score we use null then let us see what will happened it will show 0 as output
+
+ // if in score we use undefined then let us see what will happened it will show Nan as output
+
+ // if in score we use any booleann value then let us see what will happened it will show 1 as output if false then 0 as output
+
+ let isLoggedIn = 1
+
+ let booleannIsLoggedIn = Boolean(isLoggedIn)
+ // console.log(booleannIsLoggedIn)
+
+ // if 1 is true
+ // ntg written then its false
+ // "Abhay" then also true 
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(typeof stringNumber)
+ // here the number is connected to String as we used the letstringNumber opertioins 
+
+
+// 
