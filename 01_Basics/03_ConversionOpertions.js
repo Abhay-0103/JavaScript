8158cfebea03ee38fcf54ebeab98f38ce5f55b65
Abhay Singh <ab0321054@gmail.com>
2024-10-11 07:02:40 +0000

    Both Done

--- 01_Basics/03_ConversionOpertions.js
+++ 01_Basics/03_ConversionOpertions.js
@@ -1,49 +1,112 @@
-let score = true
- // if i use " " the number it will be string 
-
-
+let score = true;
+// if i use " " the number it will be string
 
 // console.log(typeof score);
 // console.log(typeof(score));
 
-
-
 // now when we used " " it is now a string then now we will convert it into number by using operterions used below
-
 
 // let valueInNumber = Number(score)
 
 // console.log(typeof valueInNumber)
 
-
 // now when we use "33abc" it will show the same number so now we will check again
-
 
 // console.log(valueInNumber)
 
+// NaN measn Not a number
 
- // NaN measn Not a number 
+// if in score we use null then let us see what will happened it will show 0 as output
 
- // if in score we use null then let us see what will happened it will show 0 as output
+// if in score we use undefined then let us see what will happened it will show Nan as output
 
- // if in score we use undefined then let us see what will happened it will show Nan as output
+// if in score we use any booleann value then let us see what will happened it will show 1 as output if false then 0 as output
 
- // if in score we use any booleann value then let us see what will happened it will show 1 as output if false then 0 as output
+let isLoggedIn = 1;
 
- let isLoggedIn = 1
+let booleannIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleannIsLoggedIn)
 
- let booleannIsLoggedIn = Boolean(isLoggedIn)
- // console.log(booleannIsLoggedIn)
+// if 1 is true
+// ntg written then its false
+// "Abhay" then also true
 
- // if 1 is true
- // ntg written then its false
- // "Abhay" then also true 
+let someNumber = 33;
 
- let someNumber = 33
-
- let stringNumber = String(someNumber)
- console.log(typeof stringNumber)
- // here the number is connected to String as we used the letstringNumber opertioins 
+let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// here the number is connected to String as we used the letstringNumber opertioins
 
 
-// 
+
+// ************ Operations ***********
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// here the given number is converted to -number by the operations
+
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); for power
+// console.log(2/2);
+// console.log(2%2); module
+
+// this are simple arthametic opertions
+
+
+
+// let str1 = "Abhay"
+// let str2 = " Singh"
+// let str3 = str1+str2;
+// console.log(str3);
+ 
+// basic string for adding 
+
+// complex string now below
+
+// console.log("1" +2);
+// console.log(1 + "2");
+
+// here we are getting both output as 12 
+
+// console.log("1" + 2 + 2);
+// here we get 122
+// but now 
+// console.log(1 + 2 + "2");
+// now we are getting output as 32
+
+// console.log(3 + 4) * 5 % 3);
+
+
+
+//console.log(true); 
+// it will come true only
+
+
+//console.log(+true);
+// it will come as 1 but not to use this complent code
+
+
+// console.log(+"");
+// it will come 0 as false  but but not to use 
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+// not to use this code also bcz its no use when all he value is same as the num1 , num2 , num3 so no need to use
+
+
+let gameCounter  = 100
+gameCounter++;
+console.log(gameCounter)
+// anything++ pre inceriment 
+// ++anything post inceriment
+
+// for extra prefix and postfix
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
