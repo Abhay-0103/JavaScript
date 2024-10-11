b850125461f02ff913015002bf8ca254cb60dc20
Abhay Singh <ab0321054@gmail.com>
2024-10-11 07:27:42 +0000

    Both Done

--- 01_Basics/03_ConversionOpertions.js
+++ 01_Basics/03_ConversionOpertions.js
@@ -37,18 +37,13 @@ let stringNumber = String(someNumber);
 // console.log(typeof stringNumber);
 // here the number is connected to String as we used the letstringNumber opertioins
 
-
-
 // ************ Operations ***********
 
-
-let value = 3
-let negValue = -value
+let value = 3;
+let negValue = -value;
 // console.log(negValue);
 
 // here the given number is converted to -number by the operations
-
-
 
 // console.log(2+2);
 // console.log(2-2);
@@ -59,53 +54,46 @@ let negValue = -value
 
 // this are simple arthametic opertions
 
-
-
 // let str1 = "Abhay"
 // let str2 = " Singh"
 // let str3 = str1+str2;
 // console.log(str3);
- 
-// basic string for adding 
+
+// basic string for adding
 
 // complex string now below
 
 // console.log("1" +2);
 // console.log(1 + "2");
 
-// here we are getting both output as 12 
+// here we are getting both output as 12
 
 // console.log("1" + 2 + 2);
 // here we get 122
-// but now 
+// but now
 // console.log(1 + 2 + "2");
 // now we are getting output as 32
 
 // console.log(3 + 4) * 5 % 3);
 
-
-
-//console.log(true); 
+//console.log(true);
 // it will come true only
-
 
 //console.log(+true);
 // it will come as 1 but not to use this complent code
 
-
 // console.log(+"");
-// it will come 0 as false  but but not to use 
+// it will come 0 as false  but but not to use
 
 // let num1, num2, num3
 
 // num1 = num2 = num3 = 2 + 2
 // not to use this code also bcz its no use when all he value is same as the num1 , num2 , num3 so no need to use
 
-
-let gameCounter  = 100
+let gameCounter = 100;
 gameCounter++;
-console.log(gameCounter)
-// anything++ pre inceriment 
+console.log(gameCounter);
+// anything++ pre inceriment
 // ++anything post inceriment
 
 // for extra prefix and postfix
