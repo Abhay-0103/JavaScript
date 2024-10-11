d883066a7f8425226032852a873109a64faaabbd
Abhay Singh <ab0321054@gmail.com>
2024-10-11 06:08:15 +0000

    DataType

--- 01_Basics/02_DataType.js
+++ 01_Basics/02_DataType.js
@@ -1,8 +1,28 @@
 "use strict"; // use new javaScript version
 
 // alert(3 +3) // we are using node.js, not browser 
-// so it will not run
+//  so it will not run
  
 console.log(3+3); 
 
 console.log("Abhay")
+
+
+let name = "Abhay"
+let age = 18
+let isloggedIn = false
+
+
+// number => 2 to power 53
+// bigint used for new numbers
+// string => ""
+// boolean => true/false
+// null => is and Standalone value
+// undefined => value is not given
+
+// symbol =>  are unique
+
+console.log(typeof undefined) // undefined
+console.log( typeof null) // object
+
+
