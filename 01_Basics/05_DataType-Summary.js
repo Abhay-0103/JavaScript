82a4594fae611ed5fbb44e8bb917c32ff1f0a597
Abhay Singh <ab0321054@gmail.com>
2024-10-13 05:29:01 +0000

    Summary

---
+++ 01_Basics/05_DataType-Summary.js
@@ -0,0 +1,35 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undfined, symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+//symbol
+// const id = symbol('123')
+// const anotherId = symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 4363343333665434323n
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Ben10","Ash","Luffy"];
+let myobj = {
+    name: "Abhay",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello Bhai");
+}
+
+console.log(typeof heros);
