366fd8b7773371ba9cd1464f8aebf63b0f2ddd78
Abhay Singh <ab0321054@gmail.com>
2024-11-21 12:40:42 +0000

    Fumction

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -53,3 +53,30 @@ console.log(loginUserMessage())
 console.log(loginUserMessage("Abhay")) // overwritting 
 
 //********************************************************************************
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Abhay",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username: "yoohooo",
+    price: 500
+})
+
+const myNewArray = [200,400,100,700]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,400,500,1000]))
